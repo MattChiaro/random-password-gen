6bfffe5496370f363857577883d869c4ce4829fd
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-05-17 18:18:52 -0500

    created arrays of all numbers and characters

--- script.js
+++ script.js
@@ -19,3 +19,104 @@ generateBtn.addEventListener("click", writePassword);
 // do they want uppercase?
 // do they want numbers?
 // do they want special char?
+
+// ask for length of a password
+//prompt
+//save it as a variable
+//check that input is a number, and inbetween 8 and 128
+
+// confirm
+
+var abcUpper = [  //uppercase alphabet
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+]
+
+var abcLower = [ //lowercase alphabet
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+]
+
+var specialChar = [ //special characters
+  "!",
+  "$",
+  "%",
+  "*",
+  "&",
+  "'",
+  "(",
+  ")",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "["
+]
+
+var numbers = [ // single digit numbers
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9
+]
+
