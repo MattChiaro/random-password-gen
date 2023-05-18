427f4328a3b269255d3c481ca02303be3b5004b7
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-05-18 09:15:02 -0500

    added loop to re-prompt if no char selected

--- script.js
+++ script.js
@@ -47,98 +47,125 @@ function getRandomItem(array) {   // generates a random number between zero and 
 
 
 function passwordPrompt() {
-var abcUpper = [  //uppercase alphabet
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
-]
+  var abcUpper = [  //uppercase alphabet
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ]
 
-var abcLower = [ //lowercase alphabet
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
-]
+  var abcLower = [ //lowercase alphabet
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ]
 
-var specialChar = [ //special characters
-  "!",
-  "$",
-  "%",
-  "*",
-  "&",
-  "'",
-  "(",
-  ")",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "=",
-  ">",
-  "?",
-  "@",
-  "["
-]
+  var specialChar = [ //special characters
+    "!",
+    "$",
+    "%",
+    "*",
+    "&",
+    "'",
+    "(",
+    ")",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "["
+  ]
 
-var numbers = [ // single digit numbers
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9
-]
+  var numbers = [ // single digit numbers
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+  ]  
+  
+  function addCharTypes() { // series of prompts for Character Types
+    if (confirm("Would you like uppercase letters in your password?")) { //prompt for uppercase
+      newPassword.push(getRandomItem(abcUpper))
+      newPasswordChoices.push(...abcUpper)
+    }
+
+    if (confirm("Would you like lowercase letters in your password?")) { //prompt for lowercase
+      newPassword.push(getRandomItem(abcLower))
+      newPasswordChoices.push(...abcLower)
+    }
+
+    if (confirm("Would you like special characters in your password?")) { //prompt for special characters
+      newPassword.push(getRandomItem(specialChar))
+      newPasswordChoices.push(...specialChar)
+    }
+
+    if (confirm("Would you numbers in your password?")) { //prompt for numbers
+      newPassword.push(getRandomItem(numbers))
+      newPasswordChoices.push(...numbers)
+    }
+
+    if (newPassword < 1) { //
+      alert("You must select some data type for your password.");
+      addCharTypes(); //loops back to start if no data types created
+    }
+  }
   var passwordLength = prompt("How many characters/numbers would you like in your password?")
 
   if (isNaN(passwordLength)) {
@@ -151,31 +178,8 @@ var numbers = [ // single digit numbers
     passwordPrompt();
   }
 
-  if (confirm("Would you like uppercase letters in your password?")) { //prompt for uppercase
-    newPassword.push(getRandomItem(abcUpper))
-    newPasswordChoices.push(...abcUpper)
-  }
+  addCharTypes();
 
-  if (confirm("Would you like lowercase letters in your password?")) { //prompt for lowercase
-    newPassword.push(getRandomItem(abcLower))
-    newPasswordChoices.push(...abcLower)
-  }
-
-  if (confirm("Would you like special characters in your password?")) { //prompt for special characters
-    newPassword.push(getRandomItem(specialChar))
-    newPasswordChoices.push(...specialChar)
-  }
-
-  if (confirm("Would you numbers in your password?")) { //prompt for numbers
-    newPassword.push(getRandomItem(numbers))
-    newPasswordChoices.push(...numbers)
-  }
-
-  if (newPassword < 1) { //
-    alert("You must select some data type for your password.")
-
-  }
-  
   while (newPassword.length < passwordLength) {
     var addtlItem = getRandomItem(newPasswordChoices);
     newPassword.push(addtlItem)
@@ -188,6 +192,6 @@ var numbers = [ // single digit numbers
 function generatePassword() { // defines that passwordPrompt runs upon button press
   newPassword = []; // clears array (to avoid bug on second iteration of generatePassword if prompted)
   newPasswordChoices = []; // clears array (to avoid bug on second iteration of generatePassword if prompted)
-return passwordPrompt();
+  return passwordPrompt();
 }
 
